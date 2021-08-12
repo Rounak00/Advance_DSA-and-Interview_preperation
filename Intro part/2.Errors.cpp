6c448b3e412aa9aba5a57b4ff4526913e8c3955c
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-08-12 20:28:18 +0530

    commit

---
+++ Intro part/2.Errors.cpp
@@ -0,0 +1,25 @@
+// compilation process:-
+/*
+  file name a.cpp;
+
+  a.cpp -> a.out (compilation time) -> input and output (exicution time)
+*/
+
+
+// Errors:-
+// Syntax Error:- if we do a typing type mistake then its syntax error; like miss cemi-colon;
+//Segmentation Error:- Illegal access of memory. like arr[10] and if we want to access arr[11] or 4/0 also {RE of sites}
+//semantic Error:- when your statement doesnot make any sense; like 16=x;
+//Linker error:- linking fase; calling funtion but your function didnot declare; 
+// Runtime error:- when your code is running; + segmantic error
+//logical error:- errors in mathematical logics ; like x=a+b/2; -> here we forget to use brackets
+
+/*
+// * Online platform errors:-
+1. ML (memory limit):- most of the site have 256 mb memory limit (array[10^7])
+2. RE (Runtime error) :- Illegal access of memory. like arr[10] and if we want to access arr[11] or 4/0 also
+3. TLE (Time limited error):-   mainly input taking time or exicution time error
+4. Recursion errors:-
+   4.a:- TLE(time limit exceed):- time end but maybe stack not full
+   4.b:- RE (Runtime Error):- stack full but maybe time not end [memory limitexceeded error]
+*/
