2cc7c4f9bb6572585f98389106fd873058ba61a2
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-10-19 22:07:08 +0530

    commit

--- 5.3Two-pointer/1Two-sum.cpp
+++ 5.3Two-pointer/1Two-sum.cpp
@@ -19,3 +19,34 @@ int main(){
 cout<<"No element"<<" ";
 return 0;
 }
+
+/*
+optimize and efficient:
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       
+                map<int,int> m;
+                vector<int> v;
+                int n= nums.size();
+                for(int i=0;i<n;i++)
+                {
+                    
+                        int diff = target - nums[i];
+                        if(m.find(diff) != m.end())
+                        {
+                        auto p = m.find(diff);        
+                        v.push_back(p->second);
+                        v.push_back(i);
+                        }
+                        m.insert(make_pair(nums[i],i));
+                }
+          
+                return v;
+}
+};
+
+
+
+
+*/
