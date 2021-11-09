8c5fcdfe4df057039c13ca0d53b3a22a8aa72b4e
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-11-10 02:44:01 +0530

    commit

--- 19. Backtracking/1Permutation.cpp
+++ 19. Backtracking/1Permutation.cpp
@@ -31,3 +31,37 @@ int main() {
 */
 
 //Efficient:
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSafe(string str,int l, int i, int r){
+    if(l!=0 && str[l-1]=='A' && str[i]=='B')
+        return false;
+    if(r==(l+1) && str[i]=='A' && str[l]=='B')
+        return false;
+    return true;
+}
+
+void permute(string str, int l, int r){
+    
+    if(l==r){
+        cout<<str<<" ";
+        return;
+    }else{
+        for(int i=l;i<=r;i++){
+            if(isSafe(str,l,i,r)){
+                swap(str[i],str[l]);
+                permute(str,l+1,r);
+                swap(str[i],str[l]);}
+        }   
+    }
+}
+
+int main() {
+	
+	string str="ABC";
+	
+    permute(str,0,str.length()-1);      
+    return 0;  
+	
+}
--- 4. Recursion/10Permutation.cpp
+++ 4. Recursion/10Permutation.cpp
@@ -3,19 +3,46 @@
 #include<algorithm>
 using namespace std;
 
-void func(string s, int i=0){
-    if(i==s.length()-1){
-        cout<<s<<endl;
+void func(string s,vector<vector<char>>ans,int i=0){
+    if(i==s.length()){
+        ans.push_back(s);
         return;
     }
     for(int j=i;j<s.length();j++){
         swap(s[i],s[j]);
-        func(s, i+1);
+        func(s,ans, i+1);
         swap(s[j],s[i]);
     }
 }
 
 int main(){
     string s="abc";
-    func(s);
+    vector<vector<char>>ans;
+
+    func(s,ans, 0);
 }
+
+//Naive:
+/*
+
+recur(string s,  vector< vector<char>>ans,vector<char>ds,  int freq[] ){
+    if(ds.size()==s.length()){ ans.push_back(ds); return ;}
+   
+   for(int i=0;i<s.length();i++){
+       if(!freq[i]){
+           freq[i]=1;
+           ds.push_back(s[i]);
+           recur(s,ans,ds,freq);
+           freq[i]=0;
+           ds.pop_back();
+       }
+   }
+}
+main(string s){
+    vector< vector<char>>ans;
+    vector<char>ds;
+    int freq[s.length()-1]={0};
+    return recur(s,ans,ds,freq);
+    
+}
+*/
