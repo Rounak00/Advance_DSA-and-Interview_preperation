474998f5d3f586c20399ce22d3f5daf77dc6118d
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-11-11 22:05:11 +0530

    commit

--- 5. Array/9Max-sum-of-subarray.cpp
+++ 5. Array/9Max-sum-of-subarray.cpp
@@ -1,5 +1,30 @@
 //If all number is negetive then use
-max=max(maxel+a[i],a[i]);
+int maxSum(int arr[], int n)
+{
+	int res = arr[0];
 
+	int maxEnding = arr[0];
+
+	for(int i = 1; i < n; i++)
+	{
+		maxEnding = max(maxEnding + arr[i], arr[i]);
+
+		res = max(maxEnding, res);
+	}
+	
+	return res;
+}
 // naive: 2loop
 //Efficient: kadane's algo
+
+// Its also accepyted
+int sum(int a[],int n){
+	int sum=0;
+	int ans=INT_MIN;
+	for(itn i=0;i<n;i++){
+		sum+=a[i];
+		ans=max(ans,sum);
+		if(sum<0){sum=0;}
+	}
+	return ans;
+}
