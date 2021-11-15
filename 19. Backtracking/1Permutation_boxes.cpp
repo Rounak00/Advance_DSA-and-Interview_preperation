79877d4ac4013888311f64da8e11d59d51faebc1
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-11-15 07:58:56 +0530

    comit

---
+++ 19. Backtracking/1Permutation_boxes.cpp
@@ -0,0 +1,15 @@
+solve(vector<int> box, int fi, int ri ) // guess here 1 and 2 two elements, fi-first items, ri-riten items
+{
+    if(fi<=ri){
+        for(int i=0;i<box.size();i++){
+            if(box[i]==0){
+                box[i]=fi;
+                solve(box,fi+1,ri);
+                box[i]=0;
+            }
+        }
+    }
+    else{
+        print(box);
+    }
+}
