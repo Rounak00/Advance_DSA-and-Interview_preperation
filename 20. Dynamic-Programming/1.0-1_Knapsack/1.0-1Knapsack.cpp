67ffc94fabb8d7a20754859e42f00cc8fc4fdaaf
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-11-19 04:22:42 +0530

    commit

--- 20. Dynamic-Programming/1.0-1_Knapsack/1.0-1Knapsack.cpp
+++ 20. Dynamic-Programming/1.0-1_Knapsack/1.0-1Knapsack.cpp
@@ -35,6 +35,26 @@ memset(t,-1,sizeof(t));
      else if(wt[n-1]>w){return t[n][w]= knapsack(wt,val,w,n-1); }
  }
 
- //top-down:
-    
-    
+ //tabulation:
+    int t[n+1][w+1];
+    for(int i=0;i<1;i++){
+        for(int j=0;j<1;j++){
+            t[i][j]==0;
+        }
+    }
+    for(int i=1;i<n+1;i++){ 
+        for(int j=1;j<n+1;j++){  
+          if(wt[i-1]<=j)
+           {
+            t[i][j]= max(
+            (val[i-1]+t[i-1][j-wt[i-1]]),
+            t[i-1][w];
+             );
+         }
+          else t[i][j]=t[i-1][j];
+        }
+    }
+    return t[n][w];
+ 
+
+ //for dp TC: O(NW)
