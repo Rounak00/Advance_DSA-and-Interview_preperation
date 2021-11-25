68deda7e3466c111d8e939fe8a895cd786d80981
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-11-25 09:31:12 +0530

    commit

--- 20. Dynamic-Programming/4.LCS/1LCS.cpp
+++ 20. Dynamic-Programming/4.LCS/1LCS.cpp
@@ -7,7 +7,7 @@ solve(string x,string y, int n, int m){
     }
 
     if(x[n-1]==y[m-1]){
-        return solve(x,y,n-1,m-1);
+        return 1+solve(x,y,n-1,m-1);
     }
     else{
         return max(solve(x,y,n,m-1),solve(x,y,n-1,m));
@@ -27,7 +27,7 @@ solve(string x,string y, int n, int m){
     if(t[n][m]!=-1){return t[m][n]}
 
     if(x[n-1]==y[m-1]){
-        return t[n][m]=solve(x,y,n-1,m-1);
+        return t[n][m]=1+solve(x,y,n-1,m-1);
     }
     else{
         return t[n][m]=max(solve(x,y,n,m-1),solve(x,y,n-1,m));
@@ -35,3 +35,27 @@ solve(string x,string y, int n, int m){
 }
 
 //Tabulation:
+
+ solve(string x, string y, int n, int m){
+     int dp[n+1][m+1];
+    
+     for(int i=0;i<=n;i++){
+         for(int j=0;j<=m;j++){
+             if(i==0||j==0){dp[i][j]=0;}
+         }
+     }
+
+     for(int i=1;i<=n;i++){
+         for(int j=1;j<=m;j++){
+             if(x[i-1]==y[j-1]){
+               return dp[n][m]=1+dp[n-1][m-1];
+                }
+            else{
+               return dp[n][m]=max(dp[i-1][j],dp[i][j-1]);
+               }
+         }
+     }
+
+
+     return dp[n][m]; 
+ }
