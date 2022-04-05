0fd384f477c3b5a901802e819993aafa4fc27f42
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-05 23:02:23 +0530

    Create 6LCA.cpp

---
+++ 14. Tree/BST/6LCA.cpp
@@ -0,0 +1,15 @@
+
+class{ 
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        int curr = root->val; 
+        if(curr < p->val && curr < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        if(curr > p->val && curr > q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        return root;
+    }
+};
