4f64880bf9048888d613940208254c991f21e0c9
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-08 01:20:36 +0530

    commit

---
+++ 21. Trie/5Word_Search2.cpp
@@ -0,0 +1,81 @@
+struct Node{    //TrieNode
+        
+        int ends;
+        string word;
+        Node *child[26]={NULL};
+    
+     Node()    
+     { 
+        ends = 0;
+        word = "";
+    }
+    };
+class Solution {
+    public:
+    Node *root = new Node();  //root
+    
+    //Trie INSERT
+    void insert(string s)
+    {
+        Node *curr=root;
+        for(auto it:s){
+            int ind=it-'a';
+            if(!curr->child[ind]){
+                curr->child[ind]=new Node();
+            }
+            curr=curr->child[ind];
+        }
+        curr->ends+=1;
+        curr->word=s;
+    }
+    
+    void solve(vector<vector<char>>& board,int i,int j,int r,int c,vector<string>& ans,Node *curr)
+    {
+        //Base case
+        //If the trie doesn't have the current char OR cell is Visited
+        int index = board[i][j]-'a';
+        if(board[i][j]=='$' || curr->child[index]==NULL)  
+        {return;}
+        
+        curr = curr->child[index];
+        if(curr->ends > 0)
+        {
+            ans.push_back(curr->word);
+            curr->ends -=1;
+        }
+        
+        //Body
+        char ch = board[i][j];   //Store current char
+        board[i][j] = '$';  //Mark current node visited
+        
+        if(i>0)     //TOP
+            solve(board,i-1,j,r,c,ans,curr);
+        if(i<r-1)   //DOWN
+            solve(board,i+1,j,r,c,ans,curr);
+        if(j>0)     //LEFT
+            solve(board,i,j-1,r,c,ans,curr);
+        if(j<c-1)   //RIGHT
+            solve(board,i,j+1,r,c,ans,curr);
+        
+        board[i][j] = ch;    //Mark current node as Unvisited by restoring the value
+    }
+    
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {        
+        int r=board.size();
+        int c=board[0].size();
+        
+        //Insert all words in TRIE
+        for(auto it:words)
+            insert(it);
+        
+        //Now search words
+        vector<string> ans;
+        for(int i=0;i<r;++i)
+        {
+            for(int j=0;j<c;++j)
+                solve(board,i,j,r,c,ans,root);
+        }
+        return ans;
+    }
+};
