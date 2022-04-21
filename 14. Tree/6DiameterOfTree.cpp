ea6febaf6b43155372cc00935b30d074ddebfae1
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-21 16:58:18 +0530

    commit

--- 14. Tree/6DiameterOfTree.cpp
+++ 14. Tree/6DiameterOfTree.cpp
@@ -1,14 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//naive : TC:O(N^2)
+class Solution {
+public:
+    int height(TreeNode *root){
+        if(!root)
+            return 0;
+        
+        return 1 + max(height(root->left),height(root->right));
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root)
+            return 0; 
+        
+        int d1 =  height(root->left)+height(root->right);
+        
+        int d2 = diameterOfBinaryTree(root->left);
+        int d3 = diameterOfBinaryTree(root->right);
+        
+        return max(d1, max(d2,d3));
+    }
+};
+//Optimize: TC: O(N)
 class Solution {
 public:
     int check(TreeNode* node,int &maxi){
--- 20. Dynamic-Programming/2_DP_on_Grids/6Cherry_PickUP.cpp
+++ 20. Dynamic-Programming/2_DP_on_Grids/6Cherry_PickUP.cpp
@@ -1,3 +1,30 @@
+//Recursion:
+class Solution {
+public:
+   
+    int solve(int r1,int c1, int c2,vector<vector<int>>&a, int n,vector<vector<vector<int>>>&dp){
+        int r2=r1+c1-c2;
+        if(r1>=n||r2>=n||c1>=n||c2>=n||a[r1][c1]==-1||a[r2][c2]==-1){return -1e9;}
+        
+        
+        if(r1==n-1&&c1==n-1){return a[r1][c1];}
+        
+        int ans=a[r1][c1];
+        if(c1!=c2)ans+=a[r2][c2];
+        int temp=max(solve(r1,c1+1,c2+1,a,n,dp),solve(r1+1,c1,c2,a,n,dp));
+        temp=max(temp,solve(r1,c1+1,c2,a,n,dp));
+        temp=max(temp,solve(r1+1,c1,c2+1,a,n,dp));
+        ans+=temp;
+        return ans;
+        
+    }
+    int cherryPickup(vector<vector<int>>& a) {
+       vector<vector<vector<int>>>dp(51,vector<vector<int>>(51,vector<int>(51,-1)));
+        int n=a.size();
+        return max(0,solve(0,0,0,a,n,dp));
+    }
+};
+
 //Memoization: TC: O(N X N X N)*4 sc:O(N) + O(50+50+0) 
 class Solution {
 public:
