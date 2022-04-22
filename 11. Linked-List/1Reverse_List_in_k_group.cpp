9b1d9d986a891e3dcdda4874da434969e1399830
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-22 05:35:27 +0100

    Update 1Reverse_List_in_k_group.cpp

--- 11. Linked-List/1Reverse_List_in_k_group.cpp
+++ 11. Linked-List/1Reverse_List_in_k_group.cpp
@@ -36,3 +36,38 @@ Node *reverseK(Node *head,int k){
     }
     return head;
 }
+
+//Striver
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || k==1)return head;
+        
+        ListNode *dummy=new ListNode(-1);
+        dummy->next=head;
+        
+        ListNode *cur=dummy, *pre=dummy, *nex=dummy;
+        
+        int count=0;
+        ListNode* c=head;
+        while(c!=NULL){
+           
+            count++;
+           c=c->next;
+        }
+        
+        while(count>=k){
+            cur=pre->next;
+            nex=cur->next;
+            for(int i=1;i<k;i++){
+                cur->next=nex->next;
+                nex->next=pre->next;
+                pre->next=nex;
+                nex=cur->next;
+            }
+            pre=cur;
+            count-=k;
+        }
+        return dummy->next;
+    }
+};
