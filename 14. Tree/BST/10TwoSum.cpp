7329376ecb1130462a0fbb83cc3431979ca2226d
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-30 09:38:51 +0530

    commit

--- 14. Tree/BST/10TwoSum.cpp
+++ 14. Tree/BST/10TwoSum.cpp
@@ -7,12 +7,12 @@ public:
         pushAll(root);
     }
 
-    /** @return whether we have a next smallest number */
+    /** return whether we have a next smallest number */
     bool hasNext() {
         return !myStack.empty();
     }
 
-    /** @return the next smallest number */
+    /** return the next smallest number */
     int next() {
         TreeNode *tmpNode = myStack.top();
         myStack.pop();
---
+++ 14. Tree/BST/13BT_or_BST.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public: bool isValidBST(TreeNode* root) {
+        return check(root,LONG_MIN , LONG_MAX);
+    }
+    
+     bool check(TreeNode* root, long minVal, long maxVal) {
+        if (root == NULL) return true;
+        if (root->val >= maxVal || root->val <= minVal) return false;
+        return check(root->left, minVal, root->val) && check(root->right, root->val, maxVal);
+    }
+};
+//we can do inorder and then check in array sorted or not
+
