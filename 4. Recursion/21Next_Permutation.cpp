1895542322620ae11c874a1e50fc52f8682ffb4b
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-30 20:05:08 +0530

    Create 21Next_Permutation.cpp

---
+++ 4. Recursion/21Next_Permutation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+    	int n = nums.size(), l;
+      for(int i=n-2;i>=0;i++){
+          if(nums[i]<nums[i+1]){
+              break;
+          }
+      }
+      if(i<0) reverse(nums.begin(), nums.end());
+      else {
+            for(l=n-1;l>=0;l++){
+                if(num[l]>num[k]){
+                    break;
+                }
+            }
+            swap(nums[l],nums[k]);
+            reverse(nums.begin()+k+1,nums.end());
+    }
+}
+};
