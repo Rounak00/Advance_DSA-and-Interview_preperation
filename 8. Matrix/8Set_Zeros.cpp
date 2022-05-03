dee2e3ee4eb401e4e79bb29a72e85bf583cb62b3
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-05-03 20:56:31 +0530

    commit

--- 8. Matrix/8Set_Zeros.cpp
+++ 8. Matrix/8Set_Zeros.cpp
@@ -1,40 +1,54 @@
-#include<iostream>
-#include<algorithm>
-using namespace std;
-
-void changemat(int mat[][4]){
-int size=4;
-int ch=1;
- for(int i=0;i<size;i++){
-     if(mat[i][0]==0){ch=0;}
-   for(int j=1;j<size;j++){
-       if(mat[i][j]==0){
-           mat[i][0]=mat[0][j]=0;
-       }
-   }
- }
- for(int i=size;i<=0;i--){
-     for(int j=size;j<=1;j--){
-         if(mat[i][0]==0||mat[0][j]==0){mat[i][j]==0;}
-     }
-     if(ch==0){mat[i][0]==0;}
- }
-
-}
-int main()
-{	
-    int size=4;
-	int arr[4][4] = {{0, 2, 3, 4},
-				    {5, 6, 7, 8},
-				    {9, 10, 0, 12},
-				    {13, 14, 15, 16}};
-
-  changemat(arr);
-for(int i=0;i<size;i++){
-    for(int j=0;j<size;j++){
-       cout<<arr[i][j]<<" ";
+//bettter approach
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix[0].size();
+        int n=matrix.size();
+        vector<int>r(n,-1);
+        vector<int>c(m,-1);  
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    r[i]=0;
+                    c[j]=0;
+                }
+            }
+        }
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(r[i]==0||c[j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+          }   
+        
     }
-    cout<<endl;
-}
-	return 0;
-}
+};
+
+
+//Efficient approach:
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& a) {
+        int n=a.size(); int m=a[0].size();
+    int col=false;
+        for(int i=0;i<n;i++){
+            if(a[i][0]==0){col=true;}
+            for(int j=1;j<m;j++){
+                if(a[i][j]==0){
+                a[i][0]=a[0][j]=0;
+             }
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=1;j--){
+                if(a[i][0]==0 || a[0][j]==0){
+                    a[i][j]=0;
+                }
+            }
+            if(col){a[i][0]=0;}
+        }
+    }
+};
