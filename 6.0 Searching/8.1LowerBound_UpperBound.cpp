12bbcf84aed0d794c9c6a1471cb5d3e0925f4713
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-05-10 17:02:10 +0530

    git push

--- 6.0 Searching/8.1LowerBound_UpperBound.cpp
+++ 6.0 Searching/8.1LowerBound_UpperBound.cpp
@@ -22,13 +22,19 @@ int ub(int arr[], int n, int x)
     int h=n-1;
     while(l<=h){
         int mid=l+(h-l)/2;
-        if(arr[mid]>=x){
-           h=mid-1;
+        if(arr[mid]<=x){
+           l=mid+1;
         }
         else{
             ans=mid;
-           l=mid+1;
+           h=mid-1;
         }
     }
     return ans;
 }
+
+
+//IN STL 
+upper_bound(arr+0,arr+n,x)-arr;
+lower_bound(arr+0,arr+n,x)-arr;
+//-arr for return index bcz by default it returns itertor;
