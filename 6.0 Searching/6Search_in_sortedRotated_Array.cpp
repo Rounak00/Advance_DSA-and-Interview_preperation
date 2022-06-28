a3f03a224e9b7f1625534a3a060c0ce983dfaf27
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-06-29 04:28:57 +0530

    git push

--- 6.0 Searching/6Search_in_sortedRotated_Array.cpp
+++ 6.0 Searching/6Search_in_sortedRotated_Array.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while (l <= r) {
-            int mid = (l+r) / 2;
-            if (target == nums[mid])
-                return mid;
-            // there exists rotation; the middle element is in the left part of the array
-            if (nums[mid] > nums[r]) {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-            // there exists rotation; the middle element is in the right part of the array
-            else if (nums[mid] < nums[l]) {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
-            // there is no rotation; just like normal binary search
-            else {
-                if (target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
+    int search(vector<int>& arr, int x) {
+        int n=arr.size();
+        int low = 0, high = n - 1;
+
+	while(low <= high)
+	{
+		int mid = (low + high) / 2;
+
+		if(arr[mid] == x)
+			return mid;
+		else if(arr[low] < arr[mid])
+		{
+			if(x >= arr[low] && x < arr[mid])
+				high = mid - 1;
+			else 
+				low = mid + 1;
+		}
+		else if(arr[high]>arr[mid])
+		{
+			if(x > arr[mid] && x <= arr[high])
+            {low = mid + 1;}
+			else
+            {high = mid - 1;}
+		}
+    
+        else{
+            if(x<arr[mid]){high=mid-1;}
+            else{low=mid+1;}
         }
-        return -1;
+	}
+	
+
+	return -1;
     }
 };
