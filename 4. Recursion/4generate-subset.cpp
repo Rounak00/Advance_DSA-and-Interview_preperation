ce931ca6285e9cf3e7b09ecf8e82ba0c913ae6dd
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-07-21 01:53:44 +0530

    git push

--- 4. Recursion/4generate-subset.cpp
+++ 4. Recursion/4generate-subset.cpp
@@ -26,3 +26,25 @@ int main(){
     vector<int>v;
     func4(0,v,arr,n);
 }
+
+
+//order wise
+void subsetsUtil(vector<int>& A, vector<vector<int> >& res,
+                 vector<int>& subset, int index)
+{
+    res.push_back(subset);
+    for (int i = index; i < A.size(); i++) {
+  
+        // include the A[i] in subset.
+        subset.push_back(A[i]);
+  
+        // move onto the next element.
+        subsetsUtil(A, res, subset, i + 1);
+  
+        // exclude the A[i] from subset and triggers
+        // backtracking.
+        subset.pop_back();
+    }
+  
+    return;
+}
