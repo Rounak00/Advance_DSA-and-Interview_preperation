13fa0316f2c8e023b4d72a036189d059d33d2ac3
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-08-05 14:41:42 +0530

    Update 2Minimum_Cost_to_Cut_a_Stick.cpp

--- 20. Dynamic-Programming/7_MCM/2Minimum_Cost_to_Cut_a_Stick.cpp
+++ 20. Dynamic-Programming/7_MCM/2Minimum_Cost_to_Cut_a_Stick.cpp
@@ -21,3 +21,34 @@ public:
         return solve(1,c,cuts,dp);
     }
 };
+
+
+//Tabulation
+class Solution {
+public:
+    
+    int minCost(int n, vector<int>& cuts) {
+        
+        int c=cuts.size();
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0)) ;
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                       
+                        if(i>j){continue;}
+                        int mini=INT_MAX;
+                        
+                        for(int ind=i;ind<=j;ind++){
+                            int cost= cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                            mini=min(mini,cost);
+                        }
+                         dp[i][j]= mini;
+            }
+        }
+        
+        return dp[1][c];
+    }
+};
