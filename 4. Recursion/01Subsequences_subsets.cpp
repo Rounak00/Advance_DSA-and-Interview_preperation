7b9097b09a9187ebaa0f49f451adf020e91cdb13
Rounak00 <mukherjeerounak10@gmail.com>
2022-11-27 19:36:51 +0530

    git push

--- 4. Recursion/01Subsequences_subsets.cpp
+++ 4. Recursion/01Subsequences_subsets.cpp
@@ -8,13 +8,26 @@ void subsequence(vector<int> input, int index = 0, vector<int> output = {})
     {
         for (auto it : output)
             cout << it << " ";
-        cout << endl;
+        cout << endl; 
         return;
     }
-    subsequence(input, index + 1, output);
+    
+
+    // subsequence(input, index + 1, output);
+    // outpu.push_back(input[ind]);
+    // subsequence(input, index + 1, output); 
+
+    //including
     output.push_back(input[index]);
     subsequence(input, index + 1, output);
-}
+    // excluding
+    output.pop_back();
+    subsequence(input, index + 1, output);
+
+}              
+
+    
+    
 
 int main()
 {
