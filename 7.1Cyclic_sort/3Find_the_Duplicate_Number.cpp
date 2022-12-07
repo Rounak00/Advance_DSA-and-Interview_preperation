ff46002d399223c019d65b940355e7147a868a95
Rounak00 <mukherjeerounak10@gmail.com>
2022-12-08 02:54:25 +0530

    git push

--- 7.1Cyclic_sort/3Find_the_Duplicate_Number.cpp
+++ 7.1Cyclic_sort/3Find_the_Duplicate_Number.cpp
@@ -17,3 +17,27 @@ public:
          return -1;
     }
 };
+
+//my approach
+ class Solution {
+public:
+    int findDuplicate(vector<int>& nums)
+    {
+        
+        int i=0;
+        while(i<nums.size()){
+            
+                int correct=nums[i]-1;
+                if(nums[i]!=nums[correct]){
+                    swap(nums[i],nums[correct]);
+                }else{
+                   i++;
+                }
+           
+        }
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=j+1){return nums[j];}
+        }
+         return -1;
+    }
+};
