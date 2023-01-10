38019f1769af1fd7acdc82b2ddddd247eee4eb2e
Rounak00 <mukherjeerounak10@gmail.com>
2023-01-10 11:00:52 +0530

    git push

--- 14. Tree/2.0Preorder_Traversal.cpp
+++ 14. Tree/2.0Preorder_Traversal.cpp
@@ -61,3 +61,23 @@ public:
         return ans;
     }
 };
+
+class Solution {
+private:
+    void travel(Node* root, vector<int>& result) {
+        if (root == nullptr) {
+            return;
+        }
+        
+        result.push_back(root -> val);
+        for (Node* child : root -> children) {
+            travel(child, result);
+        }
+    }
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        travel(root, result);
+        return result;
+    }
+};
--- 14. Tree/2.1PostOrder_Traversal.cpp
+++ 14. Tree/2.1PostOrder_Traversal.cpp
@@ -74,3 +74,25 @@ public:
         return ans;
     }
 };
+
+//N-ary 
+class Solution {
+public:
+    
+    void solve(Node *root, vector<int> &ans){
+      if(root==NULL){
+          return;
+      }  
+        
+        for(Node* it: root->children){
+            solve(it,ans);
+        }
+        ans.push_back(root->val);
+    }
+    
+    vector<int> postorder(Node* root) {
+        vector<int>res;
+        solve(root,res);
+        return res;
+    }
+};
