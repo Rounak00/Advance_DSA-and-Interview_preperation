779e281824f726822b9a797de9c05090f96ea4f4
Rounak00 <mukherjeerounak10@gmail.com>
2023-01-26 01:59:02 +0530

    git push

---
+++ 21. Trie/2a_MaxXOROf2.cpp
@@ -0,0 +1,63 @@
+struct Node{
+  Node *link[2];
+  
+  bool contain(int bit){
+      return (link[bit]!=NULL);
+  }  
+  void put(int bit,Node *node){
+      link[bit]=node;
+  }  
+  Node *get(int bit){
+      return link[bit];
+  }  
+};
+
+class Trie{
+private: Node *root;    
+public:
+    Trie(){
+      root=new Node();
+    }  
+    void insert(int num){
+        Node *node=root;
+        for(int i=31;i>=0;i--){
+            int bit= (num>>i)&1;
+            if(!node->contain(bit)){
+                node->put(bit,new Node());
+            }
+            node=node->get(bit);
+        }
+    }
+    
+    int maxfind(int num){
+        Node *node=root;
+        int maxi=0;
+        for(int i=31;i>=0;i--){
+            int bit= (num>>i)&1;
+            
+            if(node->contain(!bit)){
+                node=node->get(!bit);
+                maxi= maxi | (1<<i);
+            }else{
+                node=node->get(bit);
+            }
+        }
+        return maxi;
+    }
+};
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+        for(auto it:nums){
+            trie.insert(it);
+        }
+        int maxi=0;
+        for(auto it:nums){
+            maxi=max(maxi,trie.maxfind(it));
+        }
+        return maxi;
+    }
+    
+};
