915e5b23ca9c2b22285580ed6a74f3e03f0ccf71
Rounak00 <mukherjeerounak10@gmail.com>
2023-03-19 23:33:05 +0530

    git push

--- 22. Heap/1Heap_Sort.cpp
+++ 22. Heap/1Heap_Sort.cpp
@@ -2,50 +2,38 @@
 1. make max heap first
 2. we just put max element in last and then for last again call heapyfi
 */
-heapsort(int arr[]){ 
-    for(int i=arr.size()-1;i>=1;i--){
+void heapify(int arr[], int i, int n ){
+    int lar=i;
+    int l=2*i+1;
+    int r=2*i+2;
+    
+    if(l<n && arr[lar]<arr[l]){
+        lar=l;
+    }
+    if(r<n && arr[lar]<arr[r]){
+        lar=r;
+    }
+    if(lar!=i){
+        swap(arr[lar],arr[i]);
+        heapify(arr,lar,n);
+    }
+}
+
+int main() {
+    int arr[]={-1,5,-7,6,4,8,0};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=(n-2)/2;i>=0;i--){
+        heapify(arr,i,n);
+    }
+    for(int i=n-1;i>0;i--){
         swap(arr[i],arr[0]);
-        heapify(arr,0,i);//here i send for size
+        heapify(arr,0,i);
     }
-}
-
-
-/*
-GFG code  TC: O(n log n)
-
-
-void heapify(int arr[], int n, int i)
-{
-    int largest = i; // Initialize largest as root
-    int l = 2*i + 1; // left = 2*i + 1
-    int r = 2*i + 2; // right = 2*i + 2
-
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
-
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
-
-
-    if (largest != i)
-    {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-
-
-void heapSort(int arr[], int n)
-{
     
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
+    cout<< "Your Array: ";
     
-    for (int i=n-1; i>=0; i--)
-    {  
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
 }
-*/
